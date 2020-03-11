e7b2b8210d780cc334cd0eb6594f26aabd999f60
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-03-11 14:48:09 -0400

    Update JPanelExample.java

--- JPanelExample.java
+++ JPanelExample.java
@@ -1,18 +1,30 @@
+package com.company;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class JPanelExample extends JPanel {
-    Building[] buildingArr = new Building[5];
     private Building b1 = new Building(100, 240, 3, 10);
     private Building b2 = new Building(200, 200, 4, 12);
     private Building b3 = new Building(320, 280, 2, 8);
     private Building b4 = new Building(400, 160, 3, 14);
     private Building b5 = new Building(500, 220, 2, 11);
-    private UFO ufo1 = new UFO(500, 100, this);
+
+    private UFO[] list = new UFO[5];
+
+    public JPanelExample(){
+        for (int i=0; i<list.length; i++){
+            list[i] = new UFO((i+1)*50, (i+1)*20, (int) (Math.random()*5)+1, (int) (Math.random()*5)+1, this);
+        }
+    }
 
     private void move(){
-        ufo1.move();
+        for (int i=0; i<list.length; i++)
+            for(int j=i+1; j<list.length; j++)
+                list[i].collision(list[j]);
+        for (UFO ufo:list) ufo.move();
     }
+
 
     public void paint(Graphics g){
         super.paint(g);
@@ -26,7 +38,7 @@ public class JPanelExample extends JPanel {
         b3.paint(g2d);
         b4.paint(g2d);
         b5.paint(g2d);
-        ufo1.paint(g2d);
+        for (UFO ufo:list) ufo.paint(g2d);
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.fillRect(0, 441, 1020, 600);
     }
