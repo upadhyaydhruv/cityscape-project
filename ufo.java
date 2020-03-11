c3da76ee8fdc38532116b77ccbaa1039b3e82ca6
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-03-11 14:47:29 -0400

    added random color generator

--- ufo.java
+++ ufo.java
@@ -1,3 +1,5 @@
+package com.company;
+
 import java.awt.*;
 import java.util.Timer;
 
@@ -8,38 +10,48 @@ public class UFO {
     private int yVel;
     private int yThreshold = 125;
     private JPanelExample panel;
-    int counter = 0;
-    Timer timer = new Timer();
+    private int counter = 0;
+    private static final int DIAMETER = 50;
 
-    public UFO(int x, int y, JPanelExample jpe){
+
+    public UFO(int x, int y, int xVel, int yVel, JPanelExample jpe){
         this.x = x;
         this.y = y;
-        xVel = 1;
-        yVel = 1;
+        this.xVel = xVel;
+        this.yVel = yVel;
         panel = jpe;
     }
 
     public void move(){
-        if (x<=0) xVel = 1;
-        if (y<=0) yVel = 1;
-        if (x>=panel.getWidth()-75) xVel = -1;
-        if (y>=yThreshold-10) yVel = -1;
+        if (x<=0) xVel = -xVel;
+        if (y<=0) yVel = -yVel;
+        if (x>=panel.getWidth()-75) xVel = -xVel;
+        if (y>=yThreshold-10) yVel = -yVel;
         x+=xVel;
         y+=yVel;
     }
 
+    public void collision(UFO b){
+        int dx = (x-b.x)+(xVel-b.xVel);
+        int dy = (y-b.y)+(yVel-b.yVel);
+
+        if (Math.sqrt(dx*dx+dy*dy)<=DIAMETER){
+            xVel^=b.xVel;
+            b.xVel^=xVel;
+            xVel^=b.xVel;
+
+            yVel^=b.yVel;
+            b.yVel^=yVel;
+            yVel^=b.yVel;
+        }
+
+    }
 
     public void paint(Graphics2D g){
         counter+=1;
-        g.setColor(Color.MAGENTA);
+        g.setColor(new Color((int)(Math.random()*256), (int) (Math.random()*256), (int) (Math.random()*256)));
+        g.fillOval(x+26, y-10, 20, 20);
+        g.setColor(Color.DARK_GRAY);
         g.fillOval(x, y, 70, 30);
-        Color[] colors = new Color[2];
-        colors[0] = Color.CYAN;
-        colors[1] = Color.YELLOW;
-
-        if  (counter%2==0) g.setColor(Color.YELLOW);
-        else if (counter%2!=0) g.setColor(Color.CYAN);
-
-        g.fillOval(x+26, y-5, 20, 20);
     }
 }
