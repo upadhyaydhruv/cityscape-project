0e678b229e8271bfba10fb5207110a4cf7b6a7c9
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-04-06 21:05:41 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -7,12 +7,13 @@ import java.io.*;
 
 public class Car {
     private BufferedImage img = null;
-    private int w, h;
+    int w, h;
+    private int xVel = -5;
     private int x, y;
 
     public Car(int x, int y) {
         try {
-            this.img = ImageIO.read(new File("res\\station_wagon.png"));
+            this.img = ImageIO.read(new File("D:/Dhruv/car.png"));
             w = img.getWidth();
             h = img.getHeight();
         } catch (IOException e){
@@ -20,6 +21,28 @@ public class Car {
         }
         this.x = x;
         this.y = y;
+    }
+
+    public BufferedImage horizontalflip(BufferedImage img) {
+        int w = img.getWidth();
+        int h = img.getHeight();
+        BufferedImage newImg = new BufferedImage(w, h, img.getType());
+        Graphics2D g = newImg.createGraphics();
+        g.drawImage(img, 0, 0, w, h, w, 0, 0, h, null);
+        g.dispose();
+        return newImg;
+    }
+
+    public void move(){
+        if (xVel>0 && this.x>=720) {
+            xVel = -5;
+            this.img = horizontalflip(img);
+        }
+        else if (xVel<0 && this.x<=0){
+            xVel = 5;
+            this.img = horizontalflip(img);
+        }
+        this.x+=xVel;
     }
 
     public void paint(Graphics2D g2d){
