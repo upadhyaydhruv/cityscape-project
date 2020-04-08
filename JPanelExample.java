0ad2fc6c866fa2e332f1ef77963f3f9fe05df297
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-04-08 00:32:24 -0400

    Update JPanelExample.java
    Updated JPanel class to accommodate changes to UFO and Car classes.

--- JPanelExample.java
+++ JPanelExample.java
@@ -1,7 +1,7 @@
-package com.company;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class JPanelExample extends JPanel {
     private Building b1 = new Building(100, 240, 3, 10);
@@ -9,13 +9,30 @@ public class JPanelExample extends JPanel {
     private Building b3 = new Building(320, 280, 2, 8);
     private Building b4 = new Building(400, 160, 3, 14);
     private Building b5 = new Building(500, 220, 2, 11);
-    private UFO[] list = new UFO[5];
+    private UFO[] list = new UFO[4];
     private Car car = new Car(300, 250);
+    private UFO special;
 
     public JPanelExample(){
-        for (int i=0; i<list.length; i++){
-            list[i] = new UFO((i+1)*50, (i+1)*20, (int) (Math.random()*5)+1, (int) (Math.random()*5)+1, this);
+        for (int i=0; i<list.length-1; i++){
+            list[i] = new UFO((i+1)*50, (i+1)*20, (int) (Math.random()*2)+1, (int) (Math.random()*2)+1, false, this);
         }
+        list[list.length-1] = new UFO(100,50, 1, 1, true, this);
+        special = list[list.length-1];
+        addKeyListener(new KeyListener(){
+            @Override
+            public void keyTyped(KeyEvent e){
+            }
+            @Override
+            public void keyReleased(KeyEvent e){
+                special.keyReleased(e);
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                special.keyPressed(e);
+            }
+        });
+        setFocusable(true);
     }
 
     private void move(){
@@ -24,8 +41,8 @@ public class JPanelExample extends JPanel {
             for(int j=i+1; j<list.length; j++)
                 list[i].collision(list[j]);
         for (UFO ufo:list) ufo.move();
+        special.move();
     }
-
 
     public void paint(Graphics g){
         super.paint(g);
@@ -34,15 +51,15 @@ public class JPanelExample extends JPanel {
                 RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(new Color(0,0,153));
         g2d.fillRect(0,0,1020, 600);
+        g2d.setColor(Color.LIGHT_GRAY);
+        g2d.fillRect(0, 441, 1020, 600);
         b1.paint(g2d);
         b2.paint(g2d);
         b3.paint(g2d);
         b4.paint(g2d);
         b5.paint(g2d);
         for (UFO ufo:list) ufo.paint(g2d);
-
-        g2d.setColor(Color.LIGHT_GRAY);
-        g2d.fillRect(0, 441, 1020, 600);
+        special.paint(g2d);
         car.paint(g2d);
     }
 
