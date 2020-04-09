4089a78472edea42f83a0cffd21f43794b229f27
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-04-08 23:02:30 -0400

    Update UFO.java
    Added functionality to allow UFO movement while beam is activated, and such that the car falls down when the beam is released.

--- UFO.java
+++ UFO.java
@@ -11,8 +11,7 @@ public class UFO {
     private static final int DIAMETER = 50;
     private boolean special;
     private boolean left, right, up, down;
-    public boolean beam = false;
-
+    private boolean beam = false;
 
     public UFO(int x, int y, int xVel, int yVel, boolean special, JPanelExample jpe){
         this.x = x;
@@ -62,7 +61,7 @@ public class UFO {
     public void move(){
         if (!special) {
             if (x <= 0) xVel = -xVel;
-            if (y <= 0) yVel = -yVel;
+            if (y <= 5) yVel = -yVel;
             if (x >= panel.getWidth() - 75) xVel = -xVel;
             if (y >= yThreshold - 10) yVel = -yVel;
             x += xVel;
@@ -70,16 +69,24 @@ public class UFO {
         }
 
         if (special&&beam){
-            if (Car.getX()<=this.x-10||Car.getX()>=this.x+10){
-                Car.setxVel(0);
-                Car.setyVel(-3);
-                if (Car.getY()<=this.y){
-                    Car.setyVel(0);
+            if (Car.getX()<=this.x-140&&Car.getX()>=this.x-170){
+                if (!right&&!left) {
+                    Car.setxVel(0);
                 }
+                else {
+                    if (right) {
+                        Car.setxVel(this.xVel+1);
+                    }
+                    else {
+                        Car.setxVel(-this.xVel-1);
+                    }
+                }
+                Car.setyVel(-3);
+                if (Car.getY()<=this.y-200){
+                    Car.setyVel(0);
+                    Car.removeCar() ;
+                 }
             }
-        }
-
-        else {
             if (left){
                 x-=xVel;
             }
@@ -91,6 +98,34 @@ public class UFO {
             }
             if (down){
                 y+=yVel;
+            }
+        }
+        else {
+            if (left){
+                if (x<=0){
+
+                }
+                else {
+                    x -= xVel;
+                }
+            }
+            if (right){
+                if (x>=panel.getWidth()-75){}
+                else {
+                    x += xVel;
+                }
+            }
+            if (up){
+                if (y<=5){}
+                else {
+                    y -= yVel;
+                }
+            }
+            if (down){
+                if (y>=yThreshold){}
+                else {
+                    y += yVel;
+                }
             }
         }
     }
@@ -113,7 +148,7 @@ public class UFO {
                 b.xVel = -b.xVel;
                 b.yVel = -b.yVel;
             }
-            else if (!special && b.special) {
+            else if (!special) {
                 xVel = -xVel;
                 yVel = -yVel;
             }
@@ -125,9 +160,26 @@ public class UFO {
         g.fillOval(x+26, y-10, 20, 20);
         g.setColor(Color.DARK_GRAY);
         g.fillOval(x, y, 70, 30);
-        g.setColor(Color.YELLOW);
+        if (special){
+            g.setColor(Color.RED);
+            g.fill3DRect(this.x, this.y, 20, 10, true);
+        }
         if (beam&&special){
+            g.setColor(Color.YELLOW);
             g.fillRect(this.x+25, this.y+5,20, 1200);
+        }
+        else if (!beam && special){
+            if (Car.getY()>=250&&Car.getRight()){
+                Car.setxVel(5);
+                Car.setyVel(0);
+            }
+            else if (Car.getY()>=250){
+                Car.setxVel(-5);
+                Car.setyVel(0);
+            }
+            else {
+                Car.setyVel(5);
+            }
         }
     }
 }
